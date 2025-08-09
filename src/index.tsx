f927c569bcae526de753c0a232f47c18e8a0dd83
Elies <68759092+alias313@users.noreply.github.com>
2025-08-10 00:00:28 +0200

    Moved away from the stone age

---
+++ src/index.tsx
@@ -0,0 +1,48 @@
+import { render } from 'preact';
+import { useRef, useEffect } from 'preact/hooks';
+import './style.css';
+
+export function App() {
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  useEffect(() => {
+    if (inputRef.current) inputRef.current.focus();
+
+    const handleKeyDown = (event: KeyboardEvent) => {
+      const isTypingKey =
+        event.key.length === 1 ||
+        event.key === 'Backspace' ||
+        event.key === 'Delete';
+
+      if (
+        isTypingKey &&
+        document.activeElement !== inputRef.current &&
+        inputRef.current
+      ) {
+        inputRef.current.focus();
+      }
+    };
+
+    document.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  }, []);
+
+  return (
+    <div>
+      <h1>Autofocus Input</h1>
+      <label htmlFor="focus">Email Address</label>
+      <input
+        id="focus"
+        ref={inputRef}
+        type="email"
+        placeholder="Enter your email"
+      />
+    </div>
+  );
+}
+
+render(<App />, document.getElementById('app')!);
+
